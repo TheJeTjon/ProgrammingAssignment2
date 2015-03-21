aa3c89f74aa6fecdb9e1c6f9694e4b981e76d0d5
TheJeTjon <jerry_tjon@hotmail.com>
2015-03-21 21:06:58 +0100

    Update cachematrix.R
    Final version I guess

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,18 @@ makeCacheMatrix <- function(myMatrix = matrix()) {
   # set the myInvertedMatrix to NULL, var will be used to store the the inverted Matrix    
   myInvertedMatrix <- NULL
   
-  # 1. Create the set function: sets the value of the vector and reset the value of myInvertedMatrix to NULL 
+  # 1. Create the set function: sets the value of the matrix and reset the value of myInvertedMatrix to NULL 
   set <- function(y) {
+    message ("sets the value of the matrix and reset the value of myInvertedMatrix to NULL")
     myMatrix <<- y
     myInvertedMatrix <<- NULL
   }
   
   # 2.  Create the get function: get the value of the matrix
   get <- function() myMatrix
+  {
+    message ("Gets the value of the matrix: ")
+  }
   
   # 3.  Create the setInverse function: 
   setInverse <- function(solve) myInvertedMatrix <<- solve
@@ -32,39 +36,83 @@ makeCacheMatrix <- function(myMatrix = matrix()) {
 ## returned by makeCacheMatrix above. If the inverse has already been calculated 
 ## (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  
+  ## Return a matrix that is the inverse of 'x'
+
   myMatrix <- x$getInverse()
   if(!is.null(myMatrix)) 
-    {
-      message("Cache found: getting cached data")
-      # return and exit function 
-      return(myMatrix )
+  {
+    message("Cache found: getting cached data")
+    # return and exit function 
+    return(myMatrix)
   }
   
-  message("No cached data: running solve() function ")
+  message("No cache found: running solve() function ")
+  
+  # get the stored matrix
   data <- x$get()
+  
+  # run solve() function 
   myMatrix <- solve(data, ...)
+  
+  # run setIverse function 
   x$setInverse(myMatrix)
-  myMatrix
+  
+  # return value 
+  return (myMatrix)
 }
 
-## Sample run:
-## > x = rbind(c(5, 6), c(8,9))
-## > m = makeCacheMatrix(x)
-## > m$get()
-## [,1] [,2]
-## [1,]    5    6
-## [2,]    8    9
+# function for testing 
+sampleRun1 <- function (myMatrix = matrix){
+  myrows = c(1, 2)
+  mycolumns = c(5, 6)
+  oMatrix = rbind (myrows, mycolumns)
+  m = makeCacheMatrix(oMatrix)
+  
+  message(m$get())
+  
+  message("Attemp 1: ")
+    output <- cacheSolve(m)
+  
+  message("Attemp 2: ")
+    output <- cacheSolve(m) 
+    output
+}
 
-##  cacheSolve(m)
-## No cached data: running solve() function 
-## [,1]      [,2]
-## [1,] -3.000000  2.000000
-## [2,]  2.666667 -1.666667
-## > cacheSolve(m)
-## Cache found: getting cached data
-## [,1]      [,2]
-## [1,] -3.000000  2.000000
-## [2,]  2.666667 -1.666667
+# function for testing 
+sampleRun2 <- function (myMatrix = matrix){
+  myrows = c(1, -1/4)
+  mycolumns = c(-1/4, 1)
+  oMatrix = rbind (myrows, mycolumns)
+  m = makeCacheMatrix(oMatrix)
+  message(m$get()) 
+  
+  message("Attemp 1: ")
+  output <- cacheSolve(m)
+
+  message("Attemp 2: ")
+  output <- cacheSolve(m)
+  output
+}
+
+# > source('/ProgrammingAssignment2/cachematrix.R')
+#	> sampleRun1()
+#	Gets the value of the matrix: 
+#	1526
+#	Attemp 1: 
+#	No cache found: running solve() function 
+#	Attemp 2: 
+#	Cache found: getting cached data
+#	     myrows mycolumns
+#	[1,]  -1.50      0.50
+#	[2,]   1.25     -0.25
+#	> sampleRun2()
+#	Gets the value of the matrix: 
+#	1-0.25-0.251
+#	Attemp 1: 
+#	No cache found: running solve() function 
+#	Attemp 2: 
+#	Cache found: getting cached data
+#	        myrows mycolumns
+#	[1,] 1.0666667 0.2666667
+#	[2,] 0.2666667 1.0666667
 
